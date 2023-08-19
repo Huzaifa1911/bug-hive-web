523846d231b7fb2c0e0d32dff9c4f9d3193aff4a
Huzaifa Arshad <huzaifa.arshad@emumba.com>
2023-08-19 14:38:20 +0500

    Initial Commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <Box sx={{ backgroundColor: "red" }}>
+      <Typography>Hi, First web deisgn</Typography>
+    </Box>
+  );
+};
+
+export default HomePage;
